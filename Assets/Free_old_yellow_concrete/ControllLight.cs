d92ec04e49357f011d151db845ee53b52ab4d70e
bratches <110205734+bratches@users.noreply.github.com>
2022-07-30 15:19:53 -0700

    Finished Track

---
+++ Assets/Free_old_yellow_concrete/ControllLight.cs
@@ -0,0 +1,23 @@
+﻿#region Using
+	using UnityEngine;
+	using System.Collections;
+#endregion
+
+public class ControllLight : MonoBehaviour {
+
+#region Variable
+	public Camera cam;
+	public Transform target;
+	public float speedX = 10f;
+	public float speedY = 10f;
+#endregion
+
+	#region Update
+	void Update () {
+		var mx = Input.GetAxis ("Mouse X");
+		var my = Input.GetAxis ("Mouse Y");
+		transform.Rotate (mx * speedX, my * speedY, 0);
+		//transform.LookAt (target);
+	}
+	#endregion
+}
